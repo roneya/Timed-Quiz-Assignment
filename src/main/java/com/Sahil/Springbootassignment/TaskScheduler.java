1994d6d85a0e15e714997f275e12b12c24978fce
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-05-27 12:54:05 +0530

    Update TaskScheduler.java
    copy paste and check now.

--- src/main/java/com/Sahil/Springbootassignment/TaskScheduler.java
+++ src/main/java/com/Sahil/Springbootassignment/TaskScheduler.java
@@ -23,10 +23,16 @@ public class TaskScheduler {
         LocalDateTime currentDateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedDateTime = currentDateTime.format(formatter);
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+
+        DateTimeFormatter formatters = DateTimeFormatter.ofPattern(pattern);
+        LocalDate cdt = LocalDateTime.parse(formattedDateTime, formatters);
+        
+        
 //        System.out.println(currentDateTime);
 
         for (Quiz quiz : quizzes) {
-            quiz.setActive(currentDateTime.isAfter(quiz.getStartDateAndTime()) && currentDateTime.isBefore(quiz.getEndDateAndTime()));
+            quiz.setActive(cdt.isAfter(quiz.getStartDateAndTime()) && cdt.isBefore(quiz.getEndDateAndTime()));
             quizRepository.save(quiz);
         }
     }
